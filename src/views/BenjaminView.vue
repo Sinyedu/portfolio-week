732a877b8374dc053ac05fc880bd4f2ce0bbf3bd
Simon Nyblom <simnyb01@easv365.dk>
2025-04-11 16:03:07 +0200

    idk

--- src/views/BenjaminView.vue
+++ src/views/BenjaminView.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="about">
+    <div class="benjamin">
       <h1>This is Benjamins Page </h1>
     </div>
   </template>
   
   <style>
   @media (min-width: 1024px) {
-    .about {
+    .benjamin {
       min-height: 100vh;
       display: flex;
       align-items: center;
