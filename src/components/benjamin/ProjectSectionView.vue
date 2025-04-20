fb7fbc62875198ba8c1719506c87af321e39475b
NimNim20 <benvio01@easv365.dk>
2025-04-20 17:05:13 +0200

    I honestly forgot what I did

--- src/components/benjamin/ProjectSectionView.vue
+++ src/components/benjamin/ProjectSectionView.vue
@@ -8,6 +8,8 @@
 
 </script>
 
+<!-- lang="" determines what kind of styling language you are using, that can be either css or scss -->
+<!-- scoped determines if the styling you set is global or not. Your styling will only target elements made in this view if scoped -->
 <style lang="scss" scoped>
 
 </style>
