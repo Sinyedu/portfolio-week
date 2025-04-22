fad09803b43653668bd3b68b783bdcd8ce0a459c
Simon Nyblom <simnyb01@easv365.dk>
2025-04-22 07:43:42 +0200

    Fixed descriptions

--- src/modules/getSimonProject.js
+++ src/modules/getSimonProject.js
@@ -7,7 +7,7 @@ const projects = [
     {
         id: 1,
         title: "Trapholt",
-        description: "This is our first semester project at Trapholt.",
+        description: "This is our first semester project at Trapholt. It is a museum that offers a wide range of exhibitions, events, and activities. The website is designed to be user-friendly and easy to navigate, with a clean and modern layout.",
         image: trapholtImg,
         techstack: "HTML, CSS and Bootstrap",
         link: "https://github.com/Sinyedu/Trapholt-Eksamen",
@@ -15,7 +15,7 @@ const projects = [
     {
         id: 2,
         title: "Fantasy Festival",
-        description: "This is our second semester project at Fantasy Festival.",
+        description: "This is our second semester project at Fantasy Festival. It is a festival that takes place in september. The website is designed to be user-friendly and easy to navigate, with a clean and modern layout.",
         image: fantasyfestivalImg,
         techstack: "Vue, JavaScript, Tailwind, Firebase, HTML, CSS",
         link: "https://github.com/Sinyedu/Fantasy-Festival",
@@ -24,7 +24,7 @@ const projects = [
     {
         id: 3,
         title: "Dice Game",
-        description: "A minor game, that I made in my spare time. It is a simple dice game where you can play against the computer.",
+        description: "A minor game, that I made in my spare time. It is a simple dice game where you can play against the computer. The game is designed to be user-friendly and easy to navigate, with a clean and modern layout.",
         image: dicegameImg,
         techstack: "JavaScript, HTML, CSS",
         link: "https://github.com/Sinyedu/Dice-Game"
@@ -32,7 +32,7 @@ const projects = [
     {
         id: 4,
         title: "Project Management System",
-        description: "Benjamin and I's project management system. That we made in our ADV JS elective",
+        description: "Benjamin and I's project management system. That we made in our ADV JS elective class. It is a simple project management system where you can create projects, add tasks, and assign them to team members.",
         image: projectmanagementImg,
         techstack: "Vue, JavaScript, Tailwind, Firebase, HTML, CSS",
         link: "https://github.com/NimNim20/Project-Management-System",
@@ -41,7 +41,7 @@ const projects = [
     {
         id: 5,
         title: "Green Marine",
-        description: "A simple Simon game that I made in my spare time. It is a simple game where you have to repeat the sequence of colors.",
+        description: "A website for a company that's named Green Marine. It is a company that offers a wide range of services, including boat cleaning, maintenance, and repair. The website is designed to be user-friendly and easy to navigate, with a clean and modern layout.",
         image: "path/to/image5.jpg",
         techstack: "PHP, WordPress, HTML, CSS, JavaScript",
         link: "https://github.com/NimNim20/GreenMarine",
@@ -49,7 +49,7 @@ const projects = [
     {
         id: 6,
         title: "RetroTroppen",
-        description: "A webshop built for a school project. It is a simple webshop where you can buy retro interior.",
+        description: "A webshop built for a school project. It is a simple webshop where you can buy retro interior. It is built with PHP and WordPress. The webshop is designed to be user-friendly and easy to navigate, with a clean and modern layout.",
         image: "path/to/image6.jpg",
         techstack: "PHP, WordPress, HTML, CSS, JavaScript",
         link: "https://github.com/NimNim20/RetroTroppen"
